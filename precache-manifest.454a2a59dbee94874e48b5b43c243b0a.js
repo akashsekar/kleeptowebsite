1aaf4652e409e69d4b1347567a09a7c1ccd5b1c8
akashsekar <akashsekar6378@gmail.com>
2023-12-27 20:18:00 -0500

    Updates

---
+++ precache-manifest.454a2a59dbee94874e48b5b43c243b0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3896f9ea2f1c19ba065c",
+    "url": "/static/css/main.632d0d8d.chunk.css"
+  },
+  {
+    "revision": "3896f9ea2f1c19ba065c",
+    "url": "/static/js/main.2ac1d821.chunk.js"
+  },
+  {
+    "revision": "2f252785ab0106c33ae9",
+    "url": "/static/js/runtime~main.0da4c095.js"
+  },
+  {
+    "revision": "3bcc01dd425bb57deba4",
+    "url": "/static/css/2.1d869301.chunk.css"
+  },
+  {
+    "revision": "3bcc01dd425bb57deba4",
+    "url": "/static/js/2.ed5dd704.chunk.js"
+  },
+  {
+    "revision": "a18b610acfe131f2d474",
+    "url": "/static/js/3.49b3ba72.chunk.js"
+  },
+  {
+    "revision": "785fb462e5155e138b043236ac304274",
+    "url": "/index.html"
+  }
+];
